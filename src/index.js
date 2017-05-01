b3efec21f3d8e9214c1a6d535e767fafe4fce782
nothing <nothing>
2017-05-01 19:00:33 -0400

    installed react-bootrap

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+//Add the below code to the index.js file
+//For React bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
