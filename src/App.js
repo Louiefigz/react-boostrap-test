ddbe3fe0aacd2f679f18daf18537041f80ecde24
Bridge-to-Tech <b2t@Bridge-to-Techs-MacBook-Air.local>
2017-05-02 15:29:38 -0400

    styling for divs and code example of how it works

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { Button, Nav, NavItem, MenuItem, NavDropdown, Navbar} from 'react-bootstrap';
+import { Button, Nav, NavItem, MenuItem, NavDropdown, Navbar, Grid, Col, Row} from 'react-bootstrap';
 
 
 
@@ -40,10 +40,67 @@ class App extends Component {
           </Navbar>
         );
 
+
+        const gridInstance = (
+          <Grid>
+            <Row className="show-grid">
+              <Col xs={12} md={4} className="black"><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
+              <Col xs={6} md={4} className="green"><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+              <Col xs={6} md={4} className="pink">Pink baby!</Col>
+            </Row>
+
+            <Row className="show-grid">
+              <Col xs={6} md={4} className="green"><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+              <Col xs={6} md={4} className="pink"><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+              <Col xsHidden md={4} className="orange"><code>&lt;{'Col xsHidden md={4}'} /&gt;</code></Col>
+            </Row>
+
+            <Row className="show-grid">
+              <Col xs={6} xsOffset={6} className="black"><code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code></Col>
+            </Row>
+
+            <Row className="show-grid">
+              <Col md={6} mdPush={6} className="green"><code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code></Col>
+              <Col md={6} mdPull={6} className="orange"><code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code></Col>
+            </Row>
+          </Grid>
+        );
+
+
+
+        //
+        // const gridInstance = (
+        //   <Grid>
+        //     <Row className="show-grid">
+        //       <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
+        //       <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+        //     </Row>
+        //
+        //     <Row className="show-grid">
+        //       <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+        //       <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+        //       <Col xsHidden md={4}><code>&lt;{'Col xsHidden md={4}'} /&gt;</code></Col>
+        //     </Row>
+        //
+        //     <Row className="show-grid">
+        //       <Col xs={6} xsOffset={6}><code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code></Col>
+        //     </Row>
+        //
+        //     <Row className="show-grid">
+        //       <Col md={6} mdPush={6}><code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code></Col>
+        //       <Col md={6} mdPull={6}><code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code></Col>
+        //     </Row>
+        //   </Grid>
+        // );
+
+
+
+
     return (
       <div className="App">
 
         {navbarInstance}
+        {gridInstance}
 
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
