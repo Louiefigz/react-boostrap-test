2005a22a14158811e83f7b68fbd25986508b9c4f
Louiefigz <figueroa.aaron@gmail.com>
2017-05-01 22:31:45 -0400

    importing bootstrap components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
+
+
+
+
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// import { Nav, NavItem, MenuItem, NavDropdown, Navbar} from 'react-bootstrap';
+import { Button, Nav, NavItem, MenuItem, NavDropdown, Navbar} from 'react-bootstrap';
+
+
+
+
 
 class App extends Component {
+
+
+
+
   render() {
-    // const navbarInstance = (
-    //       <Navbar>
-    //         <Navbar.Header>
-    //           <Navbar.Brand>
-    //             <a href="#">React-Bootstrap</a>
-    //           </Navbar.Brand>
-    //         </Navbar.Header>
-    //         <Nav>
-    //           <NavItem eventKey={1} href="#">Link</NavItem>
-    //           <NavItem eventKey={2} href="#">Link</NavItem>
-    //           <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-    //             <MenuItem eventKey={3.1}>Action</MenuItem>
-    //             <MenuItem eventKey={3.2}>Another action</MenuItem>
-    //             <MenuItem eventKey={3.3}>Something else here</MenuItem>
-    //             <MenuItem divider />
-    //             <MenuItem eventKey={3.4}>Separated link</MenuItem>
-    //           </NavDropdown>
-    //         </Nav>
-    //       </Navbar>
-    //     );
+    const navbarInstance = (
+          <Navbar>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="#">React-Bootstrap</a>
+              </Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+              <NavItem eventKey={1} href="#">Link</NavItem>
+              <NavItem eventKey={2} href="#">Link</NavItem>
+              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.4}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+          </Navbar>
+        );
 
     return (
       <div className="App">
+
+        {navbarInstance}
+
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -36,7 +52,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
         <button>hola</button>
+        <Button>Que Pasa?</Button>
 
       </div>
     );
